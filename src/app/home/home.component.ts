29d0f3290b41c41200f560aa484f5cca55f94990
Metin Keleş <metinkeles677@gmail.com>
2023-02-18 22:44:35 +0300

    HomecomponentDesign

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,27 +5,34 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  question:string = "";
+  question: string = '';
+  questionTitle: string = '';
   questionList: Array<any> = [];
 
-  constructor(public questionService:QuestionService, public userService:UserService){}
+  constructor(
+    public questionService: QuestionService,
+    public userService: UserService
+  ) {}
 
-  ngOnInit():void{
-    this.questionService.getQuestion().subscribe((res) =>{
+  ngOnInit(): void {
+    this.questionService.getQuestion().subscribe((res) => {
       this.questionList = res;
-    })
+    });
   }
 
-  post(){
-    this.questionService.postQuestion({
-      username:this.userService.user.username,
-      question:this.question,
-      solutions:[]
-    }).subscribe((res) => {
-      this.questionList.push(res);
-    });
+  post() {
+    this.questionService
+      .postQuestion({
+        username: this.userService.user.username,
+        questionTitle: this.questionTitle,
+        question: this.question,
+        solutions: [],
+      })
+      .subscribe((res) => {
+        this.questionList.push(res);
+      });
   }
 }
--- src/app/solutions/solutions.component.ts
+++ src/app/solutions/solutions.component.ts
@@ -6,67 +6,86 @@ import { UserService } from '../services/user.service';
 @Component({
   selector: 'app-solutions',
   templateUrl: './solutions.component.html',
-  styleUrls: ['./solutions.component.css']
+  styleUrls: ['./solutions.component.css'],
 })
 export class SolutionsComponent {
-
-  solutionText:string = "";
+  solutionText: string = '';
   questionId: any;
-  questionObj:any;
+  questionObj: any;
 
-  constructor(public questionService:QuestionService, public userService:UserService, private route:ActivatedRoute, private router:Router){}
+  constructor(
+    public questionService: QuestionService,
+    public userService: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-  ngOnInit():void {
-    this.questionId = this.route.snapshot.paramMap.get('questionid');  
+  ngOnInit(): void {
+    this.questionId = this.route.snapshot.paramMap.get('questionid');
     this.questionService.getQuestonWithId(this.questionId).subscribe((res) => {
       console.log(res);
-      this.questionObj =res;
-    })
-   }
+      this.questionObj = res;
+    });
+  }
 
-   postSolution(){
+  postSolution() {
     let solutionObj = {
-      username:this.userService.user.username,
-      solution:this.solutionText,
-      plus:[],
-      minus:[]
+      username: this.userService.user.username,
+      solution: this.solutionText,
+      plus: [],
+      minus: [],
     };
-    this.questionObj.solutions.push(solutionObj);    
+    this.questionObj.solutions.push(solutionObj);
 
-    this.questionService.updateQuestion(this.questionObj).subscribe((res) => {      
-      this.solutionText="";
-    })
-   }
+    this.questionService.updateQuestion(this.questionObj).subscribe((res) => {
+      this.solutionText = '';
+    });
+  }
 
-   returnBack(){
+  returnBack() {
     this.router.navigateByUrl('/home');
-   }
+  }
 
-   vote(index:number,point:number){    
-    if(point == 1){
-      if(!(this.questionObj.solutions[index].plus.indexOf(this.userService.user.id) >=0)){
+  vote(index: number, point: number) {
+    if (point == 1) {
+      if (
+        !(
+          this.questionObj.solutions[index].plus.indexOf(
+            this.userService.user.id
+          ) >= 0
+        )
+      ) {
         this.questionObj.solutions[index].plus.push(this.userService.user.id);
       }
-      for(let i = 0; i < this.questionObj.solutions[index].minus.length; i++){
-        if(this.questionObj.solutions[index].minus[i] == this.userService.user.id){
-          this.questionObj.solutions[index].minus.splice(i,1);
+      for (let i = 0; i < this.questionObj.solutions[index].minus.length; i++) {
+        if (
+          this.questionObj.solutions[index].minus[i] == this.userService.user.id
+        ) {
+          this.questionObj.solutions[index].minus.splice(i, 1);
         }
       }
-    }
-    else{
-      if(!(this.questionObj.solutions[index].minus.indexOf(this.userService.user.id) >=0)){
+    } else {
+      if (
+        !(
+          this.questionObj.solutions[index].minus.indexOf(
+            this.userService.user.id
+          ) >= 0
+        )
+      ) {
         this.questionObj.solutions[index].minus.push(this.userService.user.id);
-      }      
+      }
 
-      for(let i = 0; i < this.questionObj.solutions[index].plus.length; i++){
-        if(this.questionObj.solutions[index].plus[i] == this.userService.user.id){
-          this.questionObj.solutions[index].plus.splice(i,1);
+      for (let i = 0; i < this.questionObj.solutions[index].plus.length; i++) {
+        if (
+          this.questionObj.solutions[index].plus[i] == this.userService.user.id
+        ) {
+          this.questionObj.solutions[index].plus.splice(i, 1);
         }
       }
     }
 
-    this.questionService.updateQuestion(this.questionObj).subscribe((res) => {    
-      this.solutionText="";
-    })
-   }
+    this.questionService.updateQuestion(this.questionObj).subscribe((res) => {
+      this.solutionText = '';
+    });
+  }
 }
